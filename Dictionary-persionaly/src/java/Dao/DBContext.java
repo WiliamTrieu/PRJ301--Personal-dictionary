a7ae2a87fa7b9d6ceb4ef978f45c76476b0b73eb
SpringTrieu <139671763+WiliamTrieu@users.noreply.github.com>
2025-12-05 23:14:49 +0700

    fix connection

--- Dictionary-persionaly/src/java/Dao/DBContext.java
+++ Dictionary-persionaly/src/java/Dao/DBContext.java
@@ -20,12 +20,10 @@ public class DBContext {
     private static final String PASSWORD = "123"; // Thay bằng password của bạn
     
     // Connection String cho SQL Server
-    // Thêm useJVMChained=true để tránh lỗi JAXB với Java 9+
     private static final String CONNECTION_URL = 
         "jdbc:sqlserver://" + SERVER_NAME + ":" + PORT + 
         ";databaseName=" + DATABASE_NAME + 
-        ";encrypt=true;trustServerCertificate=true" +
-        ";useJVMChained=true";
+        ";encrypt=true;trustServerCertificate=true;";
     
     /**
      * Lấy kết nối đến database
